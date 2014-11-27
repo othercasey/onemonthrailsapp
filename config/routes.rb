fdcbaa48d16dde41fbcf7d64b9cc87af9b5d66a2
Casey Greene <c@seygreene.com>
2014-11-27 15:06:36 -0800

    added new page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about' => 'pages#about'
+  get 'cats' => 'pages#cats'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
