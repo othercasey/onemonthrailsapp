9e54a195294b23989c96000708eab5d837ced643
Casey Greene <c@seygreene.com>
2015-06-30 10:16:20 -0400

    adding more info to account profiles

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,4 +3,7 @@ class Pin < ActiveRecord::Base
 
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+
+	validates :description, presence: true
+	validates :image, presence: true
 end
