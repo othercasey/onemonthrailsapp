be972845b1959a6fae5d8c1baf163f66f53d72cb
Casey Greene <c@seygreene.com>
2015-06-30 11:17:50 -0400

    photo sizing

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,7 +1,7 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
 
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>", :large => "600x600>" }
+	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>", :large => "450x450>" }
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 
 	validates :description, presence: true
